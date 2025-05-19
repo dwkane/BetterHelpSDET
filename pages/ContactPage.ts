a177b45be223dc18beee5cce5745e6b90e656ba2
Dave Kane <dwkane@gmx.net>
2025-05-19 15:46:09 -0400

    Adding test coverage

--- pages/ContactPage.ts
+++ pages/ContactPage.ts
@@ -3,30 +3,105 @@ import { Page, Locator } from '@playwright/test';
 export class ContactPage {
   private page: Page;
     
+  private contactType = {
+    REGISTERED_CLIENT: 'register-client-question',
+    CURRENT_THERAPIST: 'current-therapist-question',
+    NEW_THERAPIST: 'new-therapist-question',
+    SERVICE: 'service-question',
+    BILLING: 'billing-question',
+    PRESS: 'press-question',
+    BUSINESS: 'press-question',
+    ORGANIZATION: 'organization-question-text',
+  };
+
   public locators: {
-    cookieAccept: Locator;
+    cookies: {
+        acceptButton: Locator,
+    },
+    contactForm: {
+        radioButtons: {
+            registeredClient: Locator,
+            currentTherapist: Locator,
+            newTherapist: Locator,
+            service: Locator,
+            billing: Locator,
+            press: Locator,
+            business: Locator,
+            organization: Locator,
+        },
+        firstName: Locator,
+        lastName: Locator,
+        emailAddress: Locator,
+        message: Locator,
+        agreeCheck: Locator,
+        submit: Locator,
+    },
+    
   }
 
   constructor(page: Page) {
     this.page = page;
     this.locators = {
-        cookieAccept: this.page.getByTestId('cookie-agree-button'),
-        //username: this.page.locator('#username'),
+        cookies: {
+            acceptButton: this.page.getByTestId('cookie-agree-button'),
+        },
+        contactForm: {
+            radioButtons: {
+                registeredClient: this.page.getByTestId(this.contactType.REGISTERED_CLIENT),
+                currentTherapist: this.page.getByTestId(this.contactType.CURRENT_THERAPIST), 
+                newTherapist: this.page.getByTestId(this.contactType.NEW_THERAPIST),
+                service: this.page.getByTestId(this.contactType.SERVICE),
+                billing: this.page.getByTestId(this.contactType.BILLING),
+                press: this.page.getByTestId(this.contactType.PRESS),
+                business: this.page.getByTestId(this.contactType.BUSINESS),
+                organization: this.page.getByTestId(this.contactType.ORGANIZATION),
+            },
+            firstName: this.page.getByTestId('first-name-field'),
+            lastName: this.page.getByTestId('last-name-field'),
+            emailAddress: this.page.getByTestId('email-field'),
+            message: this.page.getByTestId('message-textarea'),
+            agreeCheck: this.page.getByTestId('contact-us-terms-checkbox'),
+            submit: this.page.getByTestId('btn-primary-submit'),
+        }
     }
   }
 
-  async goto() {
+  async gotoContactPage() {
     await this.page.goto('https://www.betterhelp.com/contact/');
   }
 
   async acceptCookies() {
-    await this.locators.cookieAccept.click();
+    await this.locators.cookies.acceptButton.click();
   }
 
-  async login(username: string, password: string) {
-    // await this.usernameInput.fill(username);
-    // await this.passwordInput.fill(password);
-    // await this.loginButton.click();
+  async enterValues(
+    firstName: string,
+    lastName: string,
+    email: string,
+    message: string,
+    agree: boolean,
+  ) {
+    if (firstName) {
+        await this.locators.contactForm.firstName.fill(firstName);
+    }
+    if (lastName) {
+        await this.locators.contactForm.lastName.fill(lastName);
+    }
+    if (email) {
+        await this.locators.contactForm.emailAddress.fill(email);
+    }
+    if (message) {
+        await this.locators.contactForm.message.fill(message);
+    }
+    if (agree) {
+        this.locators.contactForm.agreeCheck.check;
+    } else {
+        this.locators.contactForm.agreeCheck.uncheck;
+    }
+  }
+
+  async submitForm() {
+    await this.locators.contactForm.submit.click();
   }
 
   async getErrorMessage() {
--- tests/contact.spec.ts
+++ tests/contact.spec.ts
@@ -2,16 +2,62 @@ import { test, expect } from '@playwright/test';
 import { ContactPage } from '../pages/ContactPage';
 
 test.beforeEach(async ({ page }) => {
-    await page.goto('https://demo.playwright.dev/todomvc');
+    const contactPage = new ContactPage(page)
+    await contactPage.gotoContactPage();
+    await contactPage.acceptCookies();
 });
 
-test.describe('Mark all as completed', () => {
-  test('should allow me to mark all items as completed', async ({ page }) => {
-    const contactPage = new ContactPage(page)
-    
-    await contactPage.goto();
-    await expect(contactPage.locators.cookieAccept).toBeVisible();
-    await contactPage.acceptCookies();
-    await expect(contactPage.locators.cookieAccept).not.toBeVisible();
-  });
+test.describe('Displayed Elements', () => {
+    test('should display all Contact Type options', async ({ page }) => {
+        const contactPage = new ContactPage(page);
+        expect(contactPage.locators.contactForm.radioButtons.registeredClient).toBeVisible;
+        expect(contactPage.locators.contactForm.radioButtons.currentTherapist).toBeVisible;
+        expect(contactPage.locators.contactForm.radioButtons.newTherapist).toBeVisible;
+        expect(contactPage.locators.contactForm.radioButtons.service).toBeVisible;
+        expect(contactPage.locators.contactForm.radioButtons.billing).toBeVisible;
+        expect(contactPage.locators.contactForm.radioButtons.press).toBeVisible;
+        expect(contactPage.locators.contactForm.radioButtons.business).toBeVisible;
+        expect(contactPage.locators.contactForm.radioButtons.organization).toBeVisible;
+    });
+
+    test('should display all Contact Form elements', async ({ page }) => {
+        const contactPage = new ContactPage(page);
+        expect(contactPage.locators.contactForm.firstName).toBeEnabled;
+        expect(contactPage.locators.contactForm.lastName).toBeEnabled;
+        expect(contactPage.locators.contactForm.emailAddress).toBeEnabled;
+        expect(contactPage.locators.contactForm.message).toBeEnabled;
+        expect(contactPage.locators.contactForm.agreeCheck).toBeEnabled;
+        expect(contactPage.locators.contactForm.agreeCheck).not.toBeChecked;
+        expect(contactPage.locators.contactForm.submit).not.toBeEnabled;
+    });
 });
+
+test.describe('Populate and Submit Contact Form', () => {
+    test('should populate values', async ({ page }) => {
+        const contactPage = new ContactPage(page);
+        await contactPage.locators.contactForm.radioButtons.registeredClient.click();
+        await contactPage.enterValues(
+            '~FirstName',
+            'lastName',
+            'emailAddress@email.com',
+            'This is a message',
+            true,
+        );
+        await contactPage.locators.contactForm.submit.click();
+    });
+});
+
+
+
+// Additional tests:
+
+/*
+Populating wth valid values
+Populating all required fields
+Submission is successful
+Populating with invalid data and checking for errors (for each field)
+Checking each required field (i.e. not populating a required field and checking for errors)
+Checking max character limit of each field
+Checking insecure input such as executing SQL command in the text field
+*/
+
